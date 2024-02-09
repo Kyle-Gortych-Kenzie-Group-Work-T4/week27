11e2b98974c69c1ba4496dd811667e2d5cddb136
kylegortych <kyle.gortych@gmail.com>
2024-02-08 21:13:14 -0500

    initializing git action

--- GroupWork/src/main/java/com/kenzie/groupwork/complianceenforcer/ComplianceEnforcer.java
+++ GroupWork/src/main/java/com/kenzie/groupwork/complianceenforcer/ComplianceEnforcer.java
@@ -17,6 +17,7 @@ import java.util.List;
 
 public class ComplianceEnforcer {
     private final CustomerService customerService;
+
     private final RingDeviceCommunicatorService ringClient;
 
     /**
